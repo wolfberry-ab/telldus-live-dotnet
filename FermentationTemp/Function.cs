69059ab07b6d1cca10cc5ee90381523835909452
Mikael Johansson <the.mikael.johansson@gmail.com>
2021-07-29 20:37:46 +0200

    chore: add interface to sensor repository

---
+++ FermentationTemp/Function.cs
@@ -0,0 +1,75 @@
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Wolfberry.TelldusLive;
+using Wolfberry.TelldusLive.Authentication;
+using Wolfberry.TelldusLive.Repositories;
+
+namespace FermentationSensors
+{
+    public static class Function 
+    {
+        [FunctionName("FermentationSensors")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("api_key", 
+            SecuritySchemeType.ApiKey, 
+            Name = "code", 
+            In = OpenApiSecurityLocationType.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, 
+            contentType: "text/plain", 
+            bodyType: typeof(string), 
+            Description = "Sensor response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, 
+                "get", 
+                Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string code = req.Query["code"];
+
+            if (!"brygg2021".Equals(code))
+            {
+                return new UnauthorizedResult();
+            }
+
+            log.LogInformation("Incoming request");
+
+            var sensorIds = new string[]
+            {
+                "1545931809", "1545931814"
+            };
+
+            var config = new TelldusOAuth1Configuration
+            {
+                AccessTokenUrl = "https://api.telldus.com/oauth/accessToken",
+                AuthorizeTokenUrl = "https://api.telldus.com/oauth/authorize",
+                RequestTokenUrl = "https://api.telldus.com/oauth/requestToken",
+                ConsumerKey = "FEHUVEW84RAFR5SP22RABURUPHAFRUNU",
+                ConsumerKeySecret = "ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF",
+                AccessToken = "66731a835637f7ea1cbaab7a34be4fa305db0b53e",
+                AccessTokenSecret = "01008b2537a3e30b99998e927f488565"
+            };
+
+            IAuthenticator authenticator = new Authenticator(config);
+            ITelldusHttpClient client = new TelldusHttpClient(authenticator);
+            ISensorRepository sensorRepository = new SensorRepository(client);
+            var sensors = await sensorRepository.GetSensorsAsync();
+            var selectedSensors = sensors
+                .Where(x => sensorIds.Contains(x.Id))
+                .ToList();
+
+            log.LogInformation($"Returning {selectedSensors.Count} sensors");
+
+            return new OkObjectResult(selectedSensors);
+        }
+    }
+}
+
--- Wolfberry.TelldusLive/Repositories/SensorRepository.cs
+++ Wolfberry.TelldusLive/Repositories/SensorRepository.cs
@@ -4,7 +4,18 @@ using Wolfberry.TelldusLive.ViewModels;
 
 namespace Wolfberry.TelldusLive.Repositories
 {
-    public class SensorRepository
+    public interface ISensorRepository
+    {
+        /// <summary>
+        /// Returns a list of all sensors associated with the current user
+        /// https://api.telldus.net/explore/sensors/list
+        /// </summary>
+        /// <param name="format">json (default) or xml</param>
+        /// <returns></returns>
+        Task<IList<Sensor>> GetSensorsAsync(string format = Constraints.JsonFormat);
+    }
+
+    public class SensorRepository : ISensorRepository
     {
         private readonly ITelldusHttpClient _httpClient;
 
@@ -13,12 +24,7 @@ namespace Wolfberry.TelldusLive.Repositories
             _httpClient = httpClient;
         }
 
-        /// <summary>
-        /// Returns a list of all sensors associated with the current user
-        /// https://api.telldus.net/explore/sensors/list
-        /// </summary>
-        /// <param name="format">json (default) or xml</param>
-        /// <returns></returns>
+        /// <inheritdoc cref="ISensorRepository"/>
         public async Task<IList<Sensor>> GetSensorsAsync(string format = Constraints.JsonFormat)
         {
             // TODO: Add all parameters
