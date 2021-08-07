4ac6cd11849000dcc60216491cddbefd26bebee5
Mikael Johansson <the.mikael.johansson@gmail.com>
2021-08-07 07:48:23 +0200

    feat: add demo console app

---
+++ Demo.Console/App.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Wolfberry.TelldusLive;
+using Wolfberry.TelldusLive.Repositories;
+using Wolfberry.TelldusLive.ViewModels;
+
+namespace Demo.Console
+{
+    public interface IApp
+    {
+        public Task Run();
+    }
+    public class App : IApp
+    {
+        private readonly ITelldusHttpClient _httpClient;
+
+        public App(ITelldusHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task Run()
+        {
+            UserRepository userRepository;
+            StatusResponse status;
+            DeviceRepository deviceRepository;
+            string resourceParameter = null;
+
+            // clients
+            var clientRepository = new ClientRepository(_httpClient);
+            var clients = await clientRepository.GetClientsAsync(resourceParameter);
+            Print(clients);
+
+            // sensors
+            var sensorRepository = new SensorRepository(_httpClient);
+            var sensors = await sensorRepository.GetSensorsAsync();
+            Print(sensors);
+            
+            // events
+            var eventRepository = new EventRepository(_httpClient);
+            var events = await eventRepository.GetEventsAsync(resourceParameter);
+            Print(events);
+            
+            // userhones":
+            userRepository = new UserRepository(_httpClient);
+            var phones = await userRepository.GetPhonesAsync();
+            Print(phones);
+            
+            // userrofile":
+            userRepository = new UserRepository(_httpClient);
+            var profile = await userRepository.GetProfileAsync();
+            Print(profile);
+            
+            // userushTest":
+            userRepository = new UserRepository(_httpClient);
+            resourceParameter = $"{phones.Phone.First().DeviceId},hej";
+            var phoneId = resourceParameter.Split(",")[0];
+            var message = resourceParameter.Split(",")[1];
+            status = await userRepository.SendPushTest(phoneId, message);
+            Print(status);
+            
+            // deviceon":
+            deviceRepository = new DeviceRepository(_httpClient);
+            status = await deviceRepository.TurnOnAsync(resourceParameter);
+            Print(status);
+            
+            // deviceoff":
+            deviceRepository = new DeviceRepository(_httpClient);
+            status = await deviceRepository.TurnOffAsync(resourceParameter);
+            Print(status);
+        }
+
+        private static void Print(object data)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            System.Console.WriteLine(json);
+        }
+    }
+}
---
+++ Demo.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Wolfberry.TelldusLive;
+using Wolfberry.TelldusLive.Authentication;
+
+namespace Demo.Console
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Running...");
+            var config = new TelldusOAuth1Configuration
+            {
+                AccessTokenUrl = "https://api.telldus.com/oauth/accessToken",
+                AuthorizeTokenUrl = "https://api.telldus.com/oauth/authorize",
+                RequestTokenUrl = "https://api.telldus.com/oauth/requestToken",
+                ConsumerKey = "FEHUVEW84RAFR5SP22RABURUPHAFRUNU",
+                ConsumerKeySecret = "ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF",
+                AccessToken = "66731a835637f7ea1cbaab7a34be4fa305db0b53e",
+                AccessTokenSecret = "01008b2537a3e30b99998e927f488565"
+            };
+
+            IAuthenticator authenticator = new Authenticator(config);
+            ITelldusHttpClient client = new TelldusHttpClient(authenticator);
+
+            var app = new App(client);
+            app.Run().GetAwaiter().GetResult();
+            System.Console.WriteLine("Done");
+            System.Console.ReadLine();
+        }
+    }
+}
+    
